c9a55282f83f3367977f861fd727c885eb0a4dfe
aymen <aymenastro@gmail.com>
2024-11-28 19:03:34 +0100

    Rename del project

---
+++ shop-aymen-david-rodrigo/src/main/java/es/iesclaradelrey/da2d1e2425/shopaymendavidrodrigo/ShopAymenDavidRodrigoApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1e2425.shopaymendavidrodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAymenDavidRodrigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAymenDavidRodrigoApplication.class, args);
+    }
+
+}
