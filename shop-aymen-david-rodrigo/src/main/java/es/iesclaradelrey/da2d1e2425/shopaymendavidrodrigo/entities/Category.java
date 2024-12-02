ff494406a665d164112d0e3e6618ec3e37371156
aymen <aymenastro@gmail.com>
2024-12-02 17:07:51 +0100

    Creamos Categorias en Entities

---
+++ shop-aymen-david-rodrigo/src/main/java/es/iesclaradelrey/da2d1e2425/shopaymendavidrodrigo/entities/Category.java
@@ -0,0 +1,17 @@
+package es.iesclaradelrey.da2d1e2425.shopaymendavidrodrigo.entities;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private UUID id;
+    private String name;
+    private String description;
+    private String image;
+}
