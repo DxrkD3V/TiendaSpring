f73bc4c2944c115397d4364330861deb02a0176e
DavidGarcia <davidgarciarubio23@gmail.com>
2025-02-26 15:40:55 +0100

    formulario, clase CreateProductDto y metodos en el impl, repo y metodos en el controlador de products lo q faltaba

---
+++ shop-aymen-david-rodrigo/src/main/java/es/iesclaradelrey/da2d1e2425/shopaymendavidrodrigo/dto/CreateProductDto.java
@@ -0,0 +1,32 @@
+package es.iesclaradelrey.da2d1e2425.shopaymendavidrodrigo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import es.iesclaradelrey.da2d1e2425.shopaymendavidrodrigo.entities.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProductDto {
+    private String name;
+
+    private String imageurl;
+    private String description;
+    private Double price;
+
+    private int stock;
+    private String manufacture;
+
+    private String motor;
+    private int hp;
+    private int maxVelocity;
+
+    private Category category;
+}
---
+++ shop-aymen-david-rodrigo/src/main/java/es/iesclaradelrey/da2d1e2425/shopaymendavidrodrigo/exceptions/AlreadyExistException.java
@@ -0,0 +1,8 @@
+package es.iesclaradelrey.da2d1e2425.shopaymendavidrodrigo.exceptions;
+
+public class AlreadyExistException extends RuntimeException {
+
+    public AlreadyExistException(String msg) {
+        super(msg);
+    }
+}
